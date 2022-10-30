45292610ae6a975c38073f3a9b2afe2974e457f1
Ori Nagar <ori.ngr@gmail.com>
2022-10-30 21:50:43 +0200

    adding-react-cypress-library

--- cypress/e2e/integration/e2e_test.cy.js
+++ cypress/e2e/integration/e2e_test.cy.js
@@ -1,50 +1,38 @@
-describe("renders the home page", () => {
-  it("renders correctly", () => {
+describe("auth and quotes", () => {
+  it("sign-in and logout succesfully", () => {
     cy.visit("/auth");
-    cy.get(".form").should("exist");
-  });
-});
-
-describe("check if sign-up form work correctly", () => {
-  it("sign-up sucsses", () => {
-    cy.visit("/auth");
-
-    /* ==== End Cypress Studio ==== */
     /* ==== Generated with Cypress Studio ==== */
+    cy.visit("http://localhost:3000/auth");
     cy.get("#flip-mode-button").click();
-    cy.get("#email").type("test@gmail.com", { force: true });
-    cy.get("#username").type("Test", { force: true });
-    cy.get("#password").type("12345678", { force: true });
-    cy.get("#sign-up-button").click({ force: true });
-    cy.get("#my-account").click();
-
+    cy.get("input.false").type("test@gmail.com");
+    cy.get('[name="username"]').type("Test");
+    cy.get('[name="password"]').type("123456789");
+    cy.get("#sign-up-submit").click({ force: true });
+    cy.get("#logout").click();
     /* ==== End Cypress Studio ==== */
   });
-});
-
-describe("check if login form work correctly", () => {
-  it("login sucsses", () => {
+  it("login succesfully", () => {
     cy.visit("/auth");
-    cy.get(".form").should("exist");
-    cy.get("#login_email").type("test@gmail.com");
-    cy.get("#login_password").type("12345678");
-    cy.get("#login-button").click();
-    cy.wait(2000);
+    cy.get('[name="login_email"]').type("test@gmail.com");
+    cy.get('[name="login_password"]').type("12345678");
+    cy.get("#login-submit").click();
   });
-});
 
-describe("adding quote", () => {
-  it(" quotes adding feature work correctly", () => {
-    cy.get(".quote-list").should("exist");
-
+  it.only("adding quotes and delete succesfully", () => {
+    cy.visit("/auth");
+    /* ==== Generated with Cypress Studio ==== */
+    cy.get('[name="login_email"]').type("test@gmail.com");
+    cy.get('[name="login_password"]').type("12345678");
+    cy.get("#login-submit").click();
     cy.get("#new-quote").click();
-    cy.get("#author").type("Test");
-    cy.get("#quote").type("Test");
-    cy.get("#add-quote").click();
-    cy.wait(1000);
-    cy.get("#my-account").click();
-    cy.get("#delete-user").click();
-    cy.get(".quote-list");
-    /* ==== End Cypress Studio ==== */
+    cy.get("#author-input").type("Test");
+    cy.get("#content-textarea").type("Testings");
+    cy.get("#add-quote-submit").click();
+    cy.findByText("-Test").should("exist");
+    cy.get("#new-quote").click();
+    cy.get("#author-input").type("Test2");
+    cy.get("#content-textarea").type("Testings2");
+    cy.get("#add-quote-submit").click();
+    cy.get(":nth-child(5) > .details > .card > .actions > .btn-warning");
   });
 });
